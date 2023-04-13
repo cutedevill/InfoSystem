8892d5f46c4604df36d6fa7cc03a2d71ed9356e1
Elizaveta Boronina <85801025+cutedevill@users.noreply.github.com>
2023-04-13 22:16:49 +0700

    Delete CatalogService directory

--- CatalogService/Models/Brand.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CatalogService.Models
-{
-    public class Brand
-    {
-        public int BrandId { get; set; }
-        public string BrandName { get; set; }
-        public string BrandDescription { get; set; }
-    }
-}
